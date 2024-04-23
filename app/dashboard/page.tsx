40efd0fdcc62f1046db7ba2455eac697d312e448
Janis Aravena <janisaravenaespinoza@gmail.com>
2024-04-23 19:15:54 -0400

    Agregar búsqueda y paginación

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -21,7 +21,7 @@ import { Suspense } from 'react';
 
   return (
     <main>
-      <h1 className={` mb-4 text-xl md:text-2xl`}>
+      <h1 className={`mb-4 text-xl md:text-2xl text-center`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
